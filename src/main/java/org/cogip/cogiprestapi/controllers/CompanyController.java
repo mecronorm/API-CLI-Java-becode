64382df17f7fb355aee0275f68cc92117e45c5ff
mecronorm <alessandropaap@hotmail.com>
2024-02-27 15:40:33 +0100

    Create a service class for Company and refactor controller

--- src/main/java/org/cogip/cogiprestapi/controllers/CompanyController.java
+++ src/main/java/org/cogip/cogiprestapi/controllers/CompanyController.java
@@ -1,43 +1,41 @@
 package org.cogip.cogiprestapi.controllers;
 
 import org.cogip.cogiprestapi.model.Company;
-import org.cogip.cogiprestapi.repositories.CompanyRepository;
+import org.cogip.cogiprestapi.services.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/companies")
 public class CompanyController {
     @Autowired
-    private CompanyRepository companyRepository;
+    private CompanyService companyService;
 
     @PostMapping
     public void storeCompany(@RequestBody Company company){
-        companyRepository.storeCompany(company);
+        companyService.storeCompany(company);
     }
 
     @GetMapping
     public List<Company> findCompanies(){
-        return companyRepository.findAllCompanies();
+        return companyService.findAllCompanies();
     }
 
     @GetMapping("/{id}")
     public Company findCompanyById(@PathVariable("id") String id){
-        return companyRepository.findCompanyById(Integer.valueOf(id));
+        return companyService.findCompanyById(Integer.valueOf(id));
     }
 
     @PutMapping("/{id}")
     public String updateCompany(@PathVariable("id") String id,@RequestBody Company company){
-        return companyRepository.updateCompany(Integer.valueOf(id), company);
+        return companyService.updateCompany(Integer.valueOf(id), company);
     }
 
     @DeleteMapping("/{id}")
     public String deleteCompany(@PathVariable("id") String id){
-        return companyRepository.deleteCompany(Integer.valueOf(id));
+        return companyService.deleteCompany(Integer.valueOf(id));
     }
 
 }
