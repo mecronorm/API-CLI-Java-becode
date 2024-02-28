32a12c85535b50bb549e709d4f2d8b904724d24a
Thibault-be <thibault.dhooghe@gmail.com>
2024-02-28 15:46:09 +0100

    Reinstate InvoiceController

--- src/main/java/org/cogip/cogiprestapi/controllers/InvoiceController.java
+++ src/main/java/org/cogip/cogiprestapi/controllers/InvoiceController.java
@@ -10,31 +10,31 @@ import java.util.List;
 @RestController
 @RequestMapping("/invoice")
 public class InvoiceController {
-    @Autowired
-    private InvoiceService invoiceService;
-
-    @PostMapping
-    public String createInvoice(@RequestBody Invoice invoice){
-        return invoiceService.createInvoice(invoice);
-    }
-
-    @PutMapping("/{id}")
-    public String updateInvoice(@PathVariable("id") String id, @RequestBody Invoice invoice){
-        return invoiceService.updateInvoice(Integer.valueOf(id), invoice);
-    }
-
-    @DeleteMapping("/{id}")
-    public String deleteInvoice(@PathVariable("id") String id){
-        return invoiceService.deleteInvoice(Integer.valueOf(id));
-    }
-
-    @GetMapping("/{id}")
-    public Invoice getInvoiceById(@PathVariable("id") String id){
-        return invoiceService.getInvoiceById(Integer.valueOf(id));
-    }
-
-    @GetMapping
-    public List<Invoice> getAllInvoice(){
-        return invoiceService.getAllInvoice();
-    }
+  @Autowired
+  private InvoiceService invoiceService;
+  
+  @PostMapping
+  public String createInvoice(@RequestBody Invoice invoice){
+    return invoiceService.createInvoice(invoice);
+  }
+  
+  @PutMapping("/{id}")
+  public String updateInvoice(@PathVariable("id") String id, @RequestBody Invoice invoice){
+    return invoiceService.updateInvoice(Integer.valueOf(id), invoice);
+  }
+  
+  @DeleteMapping("/{id}")
+  public String deleteInvoice(@PathVariable("id") String id){
+    return invoiceService.deleteInvoice(Integer.valueOf(id));
+  }
+  
+  @GetMapping("/{id}")
+  public Invoice getInvoiceById(@PathVariable("id") String id){
+    return invoiceService.getInvoiceById(Integer.valueOf(id));
+  }
+  
+  @GetMapping
+  public List<Invoice> getAllInvoice(){
+    return invoiceService.getAllInvoice();
+  }
 }
