4a6efbc95a4191225e3eb670c96991763f53f203
mecronorm <alessandropaap@hotmail.com>
2024-03-29 10:08:17 +0100

    Add dependency for spring security, and security config

---
+++ src/main/java/org/cogip/cogiprestapi/security/SecurityConfig.java
@@ -0,0 +1,42 @@
+package org.cogip.cogiprestapi.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    UserDetailsManager inMemoryUserDetailsManager(){
+        var user1 = User.withUsername("intern").password("{noop}password").roles("intern").build();
+        var user2 = User.withUsername("accountant").password("{noop}password").roles("intern", "accountant").build();
+        var user3 = User.withUsername("admin").password("{noop}password").roles("intern", "accountant", "admin").build();
+        return new InMemoryUserDetailsManager(user1,user2,user3);
+    }
+}
